211bd16a55105e40e94af79db6ca7e6baf8cd975
Amanpriya Kulkarni <amanpriya.kulkarni@gmail.com>
2023-01-25 03:57:07 -0500

    Full page design

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import ClaimsList from './claims/ClaimsList';
 
 function App() {
   return (
-    <div>
-      <header>
-        <ClaimsList></ClaimsList>
+    <div className=' bg-gray-50'>
+      <header className='navbar navbar-expand-lg shadow-md py-4 bg-violet-100 relative flex items-center w-full justify-end px-4'>
+        <img
+          src='https://uploads-ssl.webflow.com/6345b97513a38ecb660f11d1/63875cdb6749a58465f2d480_Sheer%20Health_Logo_Color-p-1080.png'
+          className='w-auto	h-6 '
+          alt='Sheer Health Logo'
+        ></img>
       </header>
+      <ClaimsList></ClaimsList>
     </div>
   );
 }
--- src/claims/ClaimSummary.js
+++ src/claims/ClaimSummary.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { format, parseISO } from 'date-fns';
 
+function ClaimSummary({ claimInfo }) {
+  return (
+    <div className='flex flex-row justify-left border-solid border-2 border-zinc-300 rounded-lg h-20 my-4 ml-2 px-4 items-center bg-white'>
+      <div className='w-60'>{formatDate(claimInfo.DateOfService)}</div>
+      {ClaimStatusItem(claimInfo.Status)}
+      <div className='w-60'>{claimInfo.Location}</div>
+      <div className='w-40'>{claimInfo.ProviderName}</div>
+      <div className='w-40'>{`$${claimInfo.PatientResponsibility.toFixed(2)}`}</div>
+
+      <button
+        className={`bg-violet-400 hover:bg-violet-200 text-white font-bold py-2 px-4 w-40 rounded-full transition-all`}
+      >
+        Manage Claim
+      </button>
+    </div>
+  );
+}
+
+export default ClaimSummary;
+
 function ClaimStatusItem(status) {
   let formattedStatusText = '';
   let textColor = 'text-gray-600';
@@ -23,30 +43,10 @@ function ClaimStatusItem(status) {
       break;
   }
 
-  const claimStatusStyle = `text-start font-bold w-40 ${textColor}`;
+  const claimStatusStyle = `font-bold w-40 ${textColor}`;
   return <div className={claimStatusStyle}>{formattedStatusText}</div>;
 }
 
 function formatDate(date) {
   return format(new Date(parseISO(date)), 'PPP');
 }
-
-function ClaimSummary({ claimInfo }) {
-  return (
-    <div className='flex flex-row justify-left border-solid border-2 border-zinc-300 rounded-lg h-20 my-4 ml-2 px-4 items-center'>
-      <div className='text-start w-60'>{formatDate(claimInfo.DateOfService)}</div>
-      {ClaimStatusItem(claimInfo.Status)}
-      <div className='text-start w-60'>{claimInfo.Location}</div>
-      <div className='text-start w-40'>{claimInfo.ProviderName}</div>
-      <div className='text-start w-40'>{`$${claimInfo.PatientResponsibility.toFixed(2)}`}</div>
-
-      <button
-        className={`bg-indigo-400 hover:bg-indigo-200 text-white font-bold py-2 px-4 w-40 rounded-full`}
-      >
-        Manage Claim
-      </button>
-    </div>
-  );
-}
-
-export default ClaimSummary;
