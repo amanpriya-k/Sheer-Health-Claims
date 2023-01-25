2894c6dff4147ea2cb6ffbeb51ca68da84de6a18
Amanpriya Kulkarni <amanpriya.kulkarni@gmail.com>
2023-01-25 05:43:44 -0500

    Done

---
+++ src/Modal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function InfoModal() {
+  return (
+    <div className='relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white'>
+      Info Modal
+    </div>
+  );
+}
+
+export default InfoModal;
--- src/claims/ClaimOverview.js
+++ src/claims/ClaimOverview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
 import initialClaimsList from '../claims.json';
@@ -6,9 +7,13 @@ import { formatClaimStatus } from '../utils/claimStatusFormatting';
 import { formatDate } from '../utils/dateFormatting';
 import ClaimServicesList from './ClaimServicesList';
 
+import questionIcon from '../icons/questionmark.png';
+
 function ClaimOverview() {
   const { claimId } = useParams();
   const currentClaim = initialClaimsList.find((claim) => claim.ID === claimId);
+
+  //   const [infoModalOpen, setInfoModalOpen] = useState(false);
 
   const {
     ID,
@@ -22,6 +27,9 @@ function ClaimOverview() {
   } = currentClaim;
   return (
     <div className='flex flex-col w-full'>
+      <Link className={`color-violet-400 font-bold w-80 ml-8 mt-6 text-left`} to={`/claims`}>
+        {'← Return to all claims'}
+      </Link>
       <h1 className='text-4xl my-6 ml-10'>{`${formatDate(DateOfService)} at ${Location}`}</h1>
 
       <div className='w-auto bg-white border-solid border-2 border-zinc-300 rounded-lg mx-8 p-4'>
@@ -53,12 +61,22 @@ function ClaimOverview() {
             </div>
           </div>
           <div className='w-1/2 flex flex-col items-center my-20'>
-            <div className='flex'>
-              <p className='font-bold'>Claim Status:&nbsp;</p>
-              {formatClaimStatus(Status)}
+            <div className='flex text-lg items-center'>
+              <p className=''>Claim Status:&nbsp;</p>
+              <p className='font-bold'>{formatClaimStatus(Status)}&nbsp;</p>
+              <img
+                className='w-6 h-6'
+                src={questionIcon}
+                onClick={() => {
+                  alert(
+                    'TBD: Should open an info modal explaining why this CTA is available and what it does.',
+                  );
+                  //   setInfoModalOpen(!infoModalOpen);
+                }}
+              ></img>
             </div>
             <button
-              className={`bg-violet-400 hover:bg-violet-200 text-white font-bold py-2 px-4 w-60 h-10 rounded-full transition-all text-center my-6`}
+              className={`bg-violet-400 hover:bg-violet-200 text-lg text-white font-bold py-2 px-4 w-60 h-15 rounded-full transition-all text-center my-6`}
               onClick={() => alert('We are working on processing your request!')}
             >
               {Status === 'denied' ? 'Appeal Claim' : 'Submit Claim'}
